e9c3653013475a6641d975ece1bd368b3eb4320c
kiango <kianogo@yahoo.dk>
2016-05-27 18:33:46 +0200

    read me

--- ML_with_R_2.R
+++ ML_with_R_2.R
@@ -2,13 +2,16 @@
 
 # requiring 'caret' library
 library(caret)
-# random num generator
+# random num generator (setting te seed make sure to output the same random sequence of numbers)
 set.seed(122515)
+
 # cols needed for training (as vector)
 featuredCols = c("ARR_DEL15", "DAY_OF_WEEK", "CARRIER", "DEST", "ORIGIN", "DEP_TIME_BLK")
+
 # create a new subset of onTimeData that only need these cols. 
 # we unchange onTimeData to make it possible to reuse it again in another way if needed.
 onTimeDataFiltered = onTimeData[,featuredCols]
+
 # split into training and testing data
 # make sure we have correct % of data rows in the training and test data
 # make sure we have the same ratio of True / False as in the ARR_DEL_15
@@ -17,6 +20,7 @@ onTimeDataFiltered = onTimeData[,featuredCols]
 # . we only want 70% of data for training
 # . not create list but only 1 item per row
 inTrainRows = createDataPartition(onTimeDataFiltered$ARR_DEL15, p=0.7, list = FALSE)
+
 # check data
 head(inTrainRows, 10)
 # 70% of onTimeData
--- README.txt
+++ README.txt
@@ -1,2 +1,3 @@
-Hello, R data cleaning
-- Some usefull methods
+R machine learning workflow using Caret library.
+
+Stepwise processing methods for data modelling. The workflow includes data preparation, algorithm selection, training model and a test of final model.
